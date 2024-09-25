446b03a9cde00aa342fa81917285824ea448290a
Y0z64 <yairprogrammer@gmail.com>
2024-09-25 03:58:51 -0600

    Added basic SEO

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,12 +1,16 @@
 import ogImageSrc from "@images/social.png";
 
+// Check if this modifes for the english version of the site
+
 export const SITE = {
-  title: "ScrewFast",
-  tagline: "Top-quality Hardware Tools",
-  description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
-  url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  title: "Maker Space",
+  tagline: "Espacio de prototipado y desarrollo",
+  description:
+    "Maker Space es un taller de prototipado y espacio colaborativo equipado con herramientas de última generación. Ofrecemos un ambiente ideal para crear, innovar y aprender, con eventos regulares y asesoría técnica personalizada. Perfecto para entusiastas de la tecnología, emprendedores y creadores que buscan llevar sus ideas a la realidad.",
+  description_short:
+    "Taller de prototipado con equipo avanzado, eventos y asesoría técnica para innovadores y creadores.",
+  url: "https://makerspacegdl.space",
+  author: "Yair Salvador Y0z64",
 };
 
 export const SEO = {
@@ -15,7 +19,7 @@ export const SEO = {
   structuredData: {
     "@context": "https://schema.org",
     "@type": "WebPage",
-    inLanguage: "en-US",
+    inLanguage: "es-MX",
     "@id": SITE.url,
     url: SITE.url,
     name: SITE.title,
@@ -30,10 +34,11 @@ export const SEO = {
 };
 
 export const OG = {
-  locale: "en_US",
+  locale: "en_MX",
   type: "website",
   url: SITE.url,
-  title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Equip your projects with ScrewFast's top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  title: `${SITE.title}: Espacio de innovación y prototipado`,
+  description:
+    "Desarrolla tus proyectos en Maker Space con equipo de última generación, asesoría técnica y eventos. Ideal para innovadores, creadores y emprendedores que buscan convertir ideas en realidad. ¡Explora nuestras instalaciones y únete a nuestra comunidad creativa!",
   image: ogImageSrc,
 };
