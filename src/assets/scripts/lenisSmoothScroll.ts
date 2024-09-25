670b0720c61da61db9beb2a27f1135b70fb6ccf5
Y0z64 <yairprogrammer@gmail.com>
2024-09-25 03:00:15 -0600

    Replacing all js with ts because im not working in this deplorable conditions

--- src/assets/scripts/lenisSmoothScroll.ts
+++ src/assets/scripts/lenisSmoothScroll.ts
@@ -5,7 +5,7 @@ import Lenis from "lenis";
 // Script to handle Lenis library settings for smooth scrolling
 const lenis = new Lenis();
 
-function raf(time) {
+function raf(time: number) {
     lenis.raf(time);
     requestAnimationFrame(raf);
 }
