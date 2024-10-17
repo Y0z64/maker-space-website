4f9949e2383254a5b9c7fd25e7ea07ba3ada1b57
Y0z64 <yairprogrammer@gmail.com>
2024-10-16 19:39:19 -0600

    Changed styles in the docs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-import colors from 'tailwindcss/colors';
+import colors from "tailwindcss/colors";
+
+const accent = {
+  200: "#4B46CB",
+  600: "#364bff",
+  900: "#182775",
+  950: "#131e4f",
+};
+
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
@@ -7,43 +15,48 @@ export default {
   ],
   darkMode: "class",
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: "#000000",
-      white: "#ffffff",
-      gray: colors.gray,
-      indigo: colors.indigo,
-      neutral: colors.neutral, // Used mainly for text color
-      background: "#191919",
-      primary: "#322BC3",
-      secondary: "#082D8F",
-      accent: "#4B46CB",
-      muted: "#CCCEF1",
-      destructive: colors.red,
-      // Old colors, came with the templated, ideally should not be used
-      orange: {
-        100: "#ffedd5",
-        200: "#fed7aa",
-        300: "#fb713b",
-        400: "#fa5a15",
-        500: "#e14d0b",
-        600: "#ea580c",
-      }, // Primary colors, used mainly for links, buttons and svg icons
-      yellow: {
-        50: "#fefce8",
-        100: "#fef9c3",
-        400: "#facc15",
-        500: "#eab308",
-      }, // Accent colors, used mainly for star color, heading and buttons
-      red: colors.red, // Used for bookmark icon
-      zinc: colors.zinc, // Used mainly for box-shadow
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: "#000000",
+        white: "#ffffff",
+        gray: colors.gray,
+        indigo: colors.indigo,
+        neutral: colors.neutral, // Used mainly for text color
+        background: "#191919",
+        primary: "#322BC3",
+        secondary: "#082D8F",
+        muted: "#CCCEF1",
+        destructive: colors.red,
+
+        // Old colors, came with the templated, ideally should not be used
+        orange: {
+          100: "#ffedd5",
+          200: "#fed7aa",
+          300: "#fb713b",
+          400: "#fa5a15",
+          500: "#e14d0b",
+          600: "#ea580c",
+        }, // Primary colors, used mainly for links, buttons and svg icons
+        yellow: {
+          50: "#fefce8",
+          100: "#fef9c3",
+          400: "#facc15",
+          500: "#eab308",
+        }, // Accent colors, used mainly for star color, heading and buttons
+        red: colors.red, // Used for bookmark icon
+        zinc: colors.zinc, // Used mainly for box-shadow
+        // Important for starlight
+        accent: colors.indigo,
+        gray: colors.zinc,
+      },
     },
-    extend: {},
   },
   plugins: [
     require("tailwindcss/nesting"),
     require("preline/plugin"),
     require("@tailwindcss/forms"),
+    require("@astrojs/starlight-tailwind"),
   ],
 };
