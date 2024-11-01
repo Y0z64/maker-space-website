79b6f6d26d289411ac2ad3658d4371782abf7ff2
Y0z64 <yairprogrammer@gmail.com>
2024-11-01 02:05:36 -0600

    Fixed locales

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,64 +3,55 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 import starlight from "@astrojs/starlight";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  // https://docs.astro.build/en/guides/images/#authorizing-remote-images
   site: "https://screwfast.uk",
   image: {
     domains: ["images.unsplash.com"],
   },
-  // i18n: {
-  //   defaultLocale: "en",
-  //   locales: ["en", "fr"],
-  //   fallback: {
-  //     fr: "en",
-  //   },
-  //   routing: {
-  //     prefixDefaultLocale: false,
-  //   },
-  // },
+  redirects: {
+    "/docs": "/welcome-to-docs",
+  },
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
   prefetch: true,
   integrations: [
     tailwind(),
     sitemap({
       i18n: {
-        defaultLocale: "en", // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
+        defaultLocale: "es",
         locales: {
-          en: "en", // The `defaultLocale` value must present in `locales` keys
-          fr: "fr",
+          es: "es",
+          en: "en",
         },
       },
     }),
     starlight({
       title: "MakerSpace docs",
       defaultLocale: "root",
-      // https://github.com/withastro/starlight/blob/main/packages/starlight/CHANGELOG.md
-      // If no Astro and Starlight i18n configurations are provided, the built-in default locale is used in Starlight and a matching Astro i18n configuration is generated/used.
-      // If only a Starlight i18n configuration is provided, an equivalent Astro i18n configuration is generated/used.
-      // If only an Astro i18n configuration is provided, the Starlight i18n configuration is updated to match it.
-      // If both an Astro and Starlight i18n configurations are provided, an error is thrown.
-      locales: {
-        root: { label: "Español", lang: "es" },
-        in: {
-          label: "English",
-          lang: "en",
-        },
-      },
-      // https://starlight.astro.build/guides/sidebar/
+      // locales: {
+      //   // Spanish as default (root) locale
+      //   root: {
+      //     label: "Español",
+      //     lang: "es",
+      //   },
+      //   // English at /en path
+      //   en: {
+      //     label: "English",
+      //     lang: "en",
+      //   },
+      // },
       sidebar: [
         {
-          label: "Quick Start Guides",
+          label: "Guías de Inicio Rápido",
           translations: {
-            de: "Schnellstartanleitungen",
-            es: "Guías de Inicio Rápido",
-            fa: "راهنمای شروع سریع",
-            fr: "Guides de Démarrage Rapide",
-            ja: "クイックスタートガイド",
-            "zh-cn": "快速入门指南",
+            en: "Quick start guides",
           },
           autogenerate: { directory: "guides" },
         },
@@ -80,7 +71,6 @@ export default defineConfig({
           autogenerate: { directory: "advanced" },
         },
       ],
-      // TODO: Crear un github donde guardar la documentacion para el maker
       social: {
         github: "https://github.com/Y0z64",
       },
@@ -112,7 +102,7 @@ export default defineConfig({
       ],
     }),
     tailwind({
-      applyBaseStyles: false
+      applyBaseStyles: false,
     }),
     compressor({
       gzip: false,
